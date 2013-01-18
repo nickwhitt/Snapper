1b7d3cf0c44ddca003292032b46a02b392fcf333
Nick Whitt <nick.whitt@gmail.com>
2013-01-18 09:48:53 -0500

    Documenting objects and methods.

--- Database.php
+++ Database.php
@@ -16,32 +16,67 @@ class Database {
 	public function __construct($db, \PDO $pdo) {
 		$this->database = $db;
 		
+		// scan all tables within the database
 		$pdo->query(sprintf('use %s', $this->database));
 		foreach ($pdo->query('show tables', \PDO::FETCH_COLUMN, 0) as $table) {
 			$this->tables[] = $table;
 		}
 	}
 	
+	/**
+	 * Lists all scanned tables
+	 *
+	 * @param void
+	 * @return array
+	 */
 	public function listTables() {
 		return $this->tables;
 	}
 	
+	/**
+	 * Prevents the table from being scanned or exported
+	 *
+	 * @param str $table
+	 * @return void
+	 */
 	public function excludeTable($table) {
 		if (!$this->isExcluded($table)) {
 			$this->excludes[] = $table;
 		}
 	}
 	
+	/**
+	 * Prevents all the given tables from being scanned or exported
+	 *
+	 * @param array $tables
+	 * @return void
+	 */
 	public function excludeTables(array $tables) {
 		foreach ($tables as $table) {
 			$this->excludeTable($table);
 		}
 	}
 	
+	/**
+	 * Tests if the given table is excluded from being scanned or exported
+	 *
+	 * @param str $table
+	 * @return bool
+	 */
 	public function isExcluded($table) {
 		return in_array($table, $this->excludes);
 	}
 	
+	/**
+	 * Exports the given table
+	 *
+	 * Utilizes mysqldump to create a file at the given filepath location.
+	 *
+	 * @param str $table
+	 * @param str $conf
+	 * @param str $path
+	 * @return void
+	 */
 	public function exportTable($table, $conf=NULL, $path=NULL) {
 		exec(sprintf(
 			'%s > %s',
@@ -50,6 +85,13 @@ class Database {
 		));
 	}
 	
+	/**
+	 * Exports all tables in the database
+	 *
+	 * @param str $conf
+	 * @param str $path
+	 * @return void
+	 */
 	public function exportTables($conf=NULL, $path=NULL) {
 		foreach ($this->tables as $table) {
 			if (!$this->isExcluded($table)) {
@@ -58,6 +100,15 @@ class Database {
 		}
 	}
 	
+	/**
+	 * Generates the mysqldump command to be executed by exportTable
+	 *
+	 * Utilizes an optional defaults file for [client] and [mysqldump] options.
+	 *
+	 * @param str $table
+	 * @param str $conf
+	 * @return str
+	 */
 	protected function callMysqldump($table, $conf=NULL) {
 		return sprintf(
 			'/usr/bin/env mysqldump %s %s',
@@ -66,6 +117,17 @@ class Database {
 		);
 	}
 	
+	/**
+	 * Creates a filepath to the table export
+	 *
+	 * Given an optional $path (defaults to the Snapper directory), builds
+	 * a filepath like:
+	 *   $path/table_name.sql
+	 *
+	 * @param str $table
+	 * @param str $path
+	 * @return str
+	 */
 	protected function destinationFile($table, $path=NULL) {
 		$this->validatePath($path);
 		return sprintf(
@@ -75,6 +137,13 @@ class Database {
 		);
 	}
 	
+	/**
+	 * Ensures the given filepath is a valid system location
+	 *
+	 * @param str $path
+	 * @param octal $mode
+	 * @return void
+	 */
 	protected function validatePath($path=NULL, $mode=0750) {
 		if (!is_null($path) && !file_exists($path)) {
 			mkdir($path, $mode, TRUE);
--- Server.php
+++ Server.php
@@ -21,6 +21,14 @@ class Server {
 		$this->conn = NULL;
 	}
 	
+	/**
+	 * Internally stores a Database object for each database on the server
+	 *
+	 * Only non-excluded databases will be stored.
+	 *
+	 * @param void
+	 * @return void
+	 */
 	public function scanDatabases() {
 		$this->databases = array();
 		foreach ($this->conn->query('show databases', \PDO::FETCH_COLUMN, 0) as $database) {
@@ -30,34 +38,79 @@ class Server {
 		}
 	}
 	
+	/**
+	 * Prevents the database from being scanned or exported
+	 *
+	 * @param str $database
+	 * @return void
+	 */
 	public function excludeDatabase($database) {
 		if (!$this->isExcluded($database)) {
 			$this->excludes[] = $database;
 		}
 	}
 	
+	/**
+	 * Prevents all the given databases from being scanned or exported
+	 *
+	 * @param array $databases
+	 * @return void
+	 */
 	public function excludeDatabases(array $databases) {
 		foreach ($databases as $database) {
 			$this->excludeDatabase($database);
 		}
 	}
 	
+	/**
+	 * Tests if the given database is excluded from being scanned or exported
+	 *
+	 * @param str $database
+	 * @return bool
+	 */
 	public function isExcluded($database) {
 		return in_array($database, $this->excludes);
 	}
 	
+	/**
+	 * Lists all scanned databases
+	 *
+	 * @param void
+	 * @return array
+	 */
 	public function listDatabases() {
 		return array_keys($this->databases);
 	}
 	
+	/**
+	 * Retrieves the internal Database object for the given database
+	 *
+	 * @param str $database
+	 * @return Snapper\Database
+	 */
 	public function getDatabase($database) {
 		return $this->databases[$database];
 	}
 	
+	/**
+	 * Exports all tables of the given database
+	 *
+	 * @param str $database
+	 * @param str $conf
+	 * @param str $path
+	 * @return void
+	 */
 	public function exportDatabase($database, $conf=NULL, $path=NULL) {
 		$this->getDatabase($database)->exportTables($conf, $this->defaultFilepath($database, $path));
 	}
 	
+	/**
+	 * Iteratively exports all tables of each scanned database
+	 *
+	 * @param str $conf
+	 * @param str $path
+	 * @return voids
+	 */
 	public function exportDatabases($conf=NULL, $path=NULL) {
 		foreach ($this->listDatabases() as $database) {
 			if (!$this->isExcluded($database)) {
@@ -66,6 +119,17 @@ class Server {
 		}
 	}
 	
+	/**
+	 * Creates a filepath to the table export location
+	 *
+	 * Given an optional $path (defaults to the Snapper directory), builds
+	 * a directory structure like:
+	 *   $path/database_name/
+	 *
+	 * @param str $database
+	 * @param str $path
+	 * @return str
+	 */
 	protected function defaultFilepath($database, $path=NULL) {
 		return sprintf(
 			'%s/%s/',
